1de6d4982381ce876fa931ee1ba36ef1a3ef1ae5
Bruno Souto <bruno.csouto@gmail.com>
2020-06-26 13:48:59 -0300

    feature: add users creation

--- src/database/migrations/1593178618595-CreateUsers.ts
+++ src/database/migrations/1593178618595-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1593132317514
-  implements MigrationInterface {
+export default class CreateUsers1593178618595 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +14,17 @@ export default class CreateAppointments1593132317514
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
-            type: 'uuid',
-            isNullable: false,
+            name: 'name',
+            type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -40,6 +42,6 @@ export default class CreateAppointments1593132317514
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
